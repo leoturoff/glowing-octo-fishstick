9b7ef3dd9107bf3289c83fe951486dd6475f5e1a
Leo Turoff <57533776+leoturoff@users.noreply.github.com>
2025-06-29 16:16:17 +0100

    Moved pytest to dev dependency group

--- src/glowing_octo_fishstick/__init__.py
+++ src/glowing_octo_fishstick/__init__.py
@@ -4,10 +4,14 @@ import random
 
 def get_quote():
     """
-    Select a random quote
+    Select a random quote from data/toast_quotes.json
     """
-    with open('data/toast_quotes.json', 'r') as file:
-        toast_quotes = json.load(file).get('toast_quotes', [])
+    try:
+        with open('data/toast_quotes.json', 'r') as file:
+            toast_quotes = json.load(file).get('toast_quotes', [])
+            
+    except FileNotFoundError as fnfe:
+        raise fnfe
     
     random_quote, = random.sample(toast_quotes, 1)
     return random_quote
