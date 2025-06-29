a5ec7ac4f389266484f99f4b6b639b17ebc73e44
Leo Turoff <57533776+leoturoff@users.noreply.github.com>
2025-06-29 16:39:25 +0100

    Added frontend files and web index

---
+++ frontend/script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('toastButton');
+const toastText = document.getElementById('toastText');
+
+button.addEventListener('click', () => {
+  toastText.textContent = ''; // Replace with your random quote logic
+});
